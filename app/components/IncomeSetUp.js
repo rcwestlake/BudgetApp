d5d3ed1ff1e1bfd833a5dc8c341449c202cf07cc
Gabi Procell <gprocell927@gmail.com>
2016-11-03 14:51:40 -0600

    Fixes ESLint errors with the exception of 4 errors

--- app/components/IncomeSetUp.js
+++ app/components/IncomeSetUp.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import firebase, { reference, signIn } from '../firebase.js';
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableHighlight,
-  ActivityIndicator,
-} from 'react-native';
-import Separator from '../Helpers/Separator';
+  } from 'react-native';
+import firebase from '../firebase.js';
 import ExpenseSetUp from './ExpenseSetUp';
 
 const styles = StyleSheet.create({
@@ -18,17 +16,17 @@ const styles = StyleSheet.create({
     marginTop: 65,
     flexDirection: 'column',
     justifyContent: 'center',
-    backgroundColor: '#48BBEC'
+    backgroundColor: '#48BBEC',
   },
   text: {
     fontSize: 18,
     color: '#111',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   buttonText: {
     fontSize: 18,
     color: '#111',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   selectedButton: {
     height: 45,
@@ -40,7 +38,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 10,
     alignSelf: 'stretch',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   button: {
     height: 45,
@@ -52,7 +50,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 10,
     alignSelf: 'stretch',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   searchInput: {
     height: 50,
@@ -62,13 +60,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'white',
     borderRadius: 8,
-    color: 'white'
+    color: 'white',
   },
-})
+});
 
 export default class IncomeSetUp extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       income: '',
@@ -76,89 +74,98 @@ export default class IncomeSetUp extends Component {
       biweekly: false,
       monthly: false,
       annually: false,
-      none: false
-    }
+      none: false,
+    };
   }
 
   handleSubmit() {
-    let user = this.props.user;
-    let { income } = this.state;
-    firebase.database().ref('users/' + user.uid).set ({
-      income: this.calculateIncome(income)
-    })
+    const user = this.props.user;
+    const { income } = this.state;
+    firebase.database().ref(`users/${user.uid}`).set({
+      income: this.calculateIncome(income),
+    });
     this.props.navigator.push({
       title: 'Recurring Expenses',
-      component: ExpenseSetUp
-    })
+      component: ExpenseSetUp,
+    });
   }
 
   calculateIncome(income) {
-    if(this.state.weekly) {
-      return income = Math.floor((income * 52) / 12);
-      console.log('weekly income', income);
+    if (this.state.weekly) {
+      const adjustedIncome = Math.floor((income * 52) / 12);
+      return adjustedIncome;
     }
-    if(this.state.biweekly) {
-      return income = Math.floor((income * 26) / 12);
+    if (this.state.biweekly) {
+      const adjustedIncome = Math.floor((income * 26) / 12);
+      return adjustedIncome;
     }
-    if(this.state.monthly) {
-      return income = Math.floor(income);
+    if (this.state.monthly) {
+      const adjustedIncome = Math.floor(income);
+      return adjustedIncome;
     }
-    if(this.state.annually) {
-      return income = Math.floor(income/12);
+    if (this.state.annually) {
+      const adjustedIncome = Math.floor(income / 12);
+      return adjustedIncome;
     }
-    if(this.state.none) {
-      return income
+    if (this.state.none) {
+      return income;
     }
   }
 
 
   render() {
-    console.log(this.props.user);
     return (
       <View style={styles.container}>
-      <Text> Income </Text>
-      <TextInput
-      style={styles.searchInput}
-      onChangeText={(text) => this.setState({ income: text })}
-      placeholder='Enter income'
-      />
-      <Text> How Often? </Text>
-      <TouchableHighlight
-        style={ this.state.weekly ? styles.selectedButton : styles.button}          underlayColor='black'
-        onPress={() => this.setState({ weekly: !this.state.weekly })}>
-        <Text style={styles.buttonText}> Weekly </Text>
-      </TouchableHighlight>
-      <TouchableHighlight
-        style={ this.state.biweekly ? styles.selectedButton : styles.button}
-        underlayColor='black'
-        onPress={() => this.setState({ biweekly: !this.state.biweekly })}>
-        <Text style={styles.buttonText}> Biweekly </Text>
-      </TouchableHighlight>
-      <TouchableHighlight
-        style={ this.state.monthly ? styles.selectedButton : styles.button}
-        underlayColor='black'
-        onPress={() => this.setState({ monthly: !this.state.monthly })}>
-        <Text style={styles.buttonText}> Monthly </Text>
-      </TouchableHighlight>
-      <TouchableHighlight
-        style={ this.state.annually ? styles.selectedButton : styles.button}
-        underlayColor='black'
-        onPress={() => this.setState({ annually: !this.state.annually })}>
-        <Text style={styles.buttonText}> Annually </Text>
-      </TouchableHighlight>
-      <TouchableHighlight
-        style={ this.state.none ? styles.selectedButton : styles.button}
-        underlayColor='black'
-        onPress={() => this.setState({ none: !this.state.none })}>
-        <Text style={styles.buttonText}> None </Text>
-      </TouchableHighlight>
-      <TouchableHighlight
-      style={styles.button}
-      underlayColor='black'
-      onPress={() => this.handleSubmit()}>
-      <Text style={styles.buttonText}> Continue </Text>
-      </TouchableHighlight>
+        <Text> Income </Text>
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={text => this.setState({ income: text })}
+          placeholder="Enter income"
+        />
+        <Text> How Often? </Text>
+        <TouchableHighlight
+          style={this.state.weekly ? styles.selectedButton : styles.button}
+          underlayColor="black"
+          onPress={() => this.setState({ weekly: !this.state.weekly })}
+        >
+          <Text style={styles.buttonText}> Weekly </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.state.biweekly ? styles.selectedButton : styles.button}
+          underlayColor="black"
+          onPress={() => this.setState({ biweekly: !this.state.biweekly })}
+        >
+          <Text style={styles.buttonText}> Biweekly </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.state.monthly ? styles.selectedButton : styles.button}
+          underlayColor="black"
+          onPress={() => this.setState({ monthly: !this.state.monthly })}
+        >
+          <Text style={styles.buttonText}> Monthly </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.state.annually ? styles.selectedButton : styles.button}
+          underlayColor="black"
+          onPress={() => this.setState({ annually: !this.state.annually })}
+        >
+          <Text style={styles.buttonText}> Annually </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.state.none ? styles.selectedButton : styles.button}
+          underlayColor="black"
+          onPress={() => this.setState({ none: !this.state.none })}
+        >
+          <Text style={styles.buttonText}> None </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.button}
+          underlayColor="black"
+          onPress={() => this.handleSubmit()}
+        >
+          <Text style={styles.buttonText}> Continue </Text>
+        </TouchableHighlight>
       </View>
-    )
+    );
   }
 }

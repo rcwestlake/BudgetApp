209b622151e53480aa7654801b87a1e8788d9026
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-04 08:06:20 -0600

    Style Welcome page, lint index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,30 @@
 'use strict';
 
-import firebase from 'firebase'
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   NavigatorIOS,
-  View
  } from 'react-native';
 import Welcome from './app/components/Welcome.js';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#111111'
+    backgroundColor: '#111111',
   },
 });
 
 export default class Application extends Component {
   render() {
-    return(
-        <NavigatorIOS
-          style={styles.container}
-          initialRoute={{
-            component: Welcome,
-            title: "Welcome",
-          }} />
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          component: Welcome,
+          title: 'Welcome',
+        }}
+      />
     );
   }
 }

fac5bb00492f7cba4b4aad675ac127d2b8f1cb52
Andrew Crist <andrew.l.crist@gmail.com>
2016-11-06 18:06:30 -0700

    Update quotes and logo

--- app/components/Quotes.js
+++ app/components/Quotes.js
@@ -1,60 +1,60 @@
 const randomQuote = () => {
   const quotesArray = [
-    'A wise man should have money in his head, but not in his heart. - Jonathan Swift',
+    'A wise man should have money in his head, but not in his heart. \n\n- Jonathan Swift',
 
-    'Wealth is the ability to fully experience life. - Henry David Thoreau',
+    'Wealth is the ability to fully experience life. \n\n- Henry David Thoreau',
 
-    'Money has never made man happy, nor will it, there is nothing in its nature to produce happiness. The more of it one has the more one wants. - Benjamin Franklin',
+    'Money has never made man happy, nor will it, there is nothing in its nature to produce happiness. The more of it one has the more one wants. \n\n- Benjamin Franklin',
 
-    'Money won\'t create success, the freedom to make it will. - Nelson Mandela',
+    'Money won\'t create success, the freedom to make it will. \n\n- Nelson Mandela',
 
-    'A little thought and a little kindness are often worth more than a great deal of money. - John Ruskin',
+    'A little thought and a little kindness are often worth more than a great deal of money. \n\n- John Ruskin',
 
-    'If saving money is wrong, I don\'t want to be right! - William Shatner',
+    'If saving money is wrong, I don\'t want to be right! \n\n- William Shatner',
 
-    'A penny saved is a penny earned. - Benjamin Franklin',
+    'A penny saved is a penny earned. \n\n- Benjamin Franklin',
 
-    'The lack of money is the root of all evil. - Mark Twain',
+    'The lack of money is the root of all evil. \n\n- Mark Twain',
 
-    'A bank is a place that will lend you money if you can prove that you don\'t need it. Bob Hope',
+    'A bank is a place that will lend you money if you can prove that you don\'t need it. \n\n- Bob Hope',
 
-    'When I was young I thought that money was the most important thing in life; now that I am old I know that it is. - Oscar Wilde',
+    'When I was young I thought that money was the most important thing in life; now that I am old I know that it is. \n\n- Oscar Wilde',
 
-    'I am happy to make money. I want to make more money, make more music, eat Big Macs and drink Budweisers. - Kid Rock',
+    'I am happy to make money. I want to make more money, make more music, eat Big Macs and drink Budweisers. \n\n- Kid Rock',
 
-    'Money is to my social existence what health is to my body. - Mason Cooley',
+    'Money is to my social existence what health is to my body. \n\n- Mason Cooley',
 
-    'Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver. - Ayn Rand',
+    'Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver. \n\n- Ayn Rand',
 
-    'There is a gigantic difference between earning a great deal of money and being rich. - Marlene Dietrich',
+    'There is a gigantic difference between earning a great deal of money and being rich. \n\n- Marlene Dietrich',
 
-    'Money is better than poverty, if only for financial reasons. - Woody Allen',
+    'Money is better than poverty, if only for financial reasons. \n\n- Woody Allen',
 
-    'A good reputation is more valuable than money. - Publilius Syrus',
+    'A good reputation is more valuable than money. \n\n- Publilius Syrus',
 
-    'Money cannot buy peace of mind. It cannot heal ruptured relationships, or build meaning into a life that has none. - Richard M. DeVos',
+    'Money cannot buy peace of mind. It cannot heal ruptured relationships, or build meaning into a life that has none. \n\n- Richard M. DeVos',
 
-    'Money makes your life easier. If you\'re lucky to have it, you\'re lucky. Robert De Niro',
+    'Money makes your life easier. If you\'re lucky to have it, you\'re lucky. \n\n- Robert De Niro',
 
-    'Where large sums of money are concerned, it is advisable to trust nobody. - Agatha Christie',
+    'Where large sums of money are concerned, it is advisable to trust nobody. \n\n- Agatha Christie',
 
-    'The safe way to double your money is to fold it over once and put it in your pocket. - Kin Hubbard',
+    'The safe way to double your money is to fold it over once and put it in your pocket. \n\n- Kin Hubbard',
 
-    'Here\'s how I think of my money - as soldiers - I send them out to war everyday. I want them to take prisoners and come home, so there\'s more of them. Kevin O\'Leary',
+    'Here\'s how I think of my money - as soldiers - I send them out to war everyday. I want them to take prisoners and come home, so there\'s more of them. \n\n- Kevin O\'Leary',
 
-    'The importance of money flows from it being a link between the present and the future. - John Maynard Keynes',
+    'The importance of money flows from it being a link between the present and the future. \n\n- John Maynard Keynes',
 
-    'Sometimes your best investments are the ones you don\'t make. Donald Trump',
+    'Sometimes your best investments are the ones you don\'t make. \n\n- Donald Trump',
 
-    'A rich man is nothing but a poor man with money. - W. C. Fields',
+    'A rich man is nothing but a poor man with money. \n\n- W. C. Fields',
 
-    'So you think that money is the root of all evil. Have you ever asked what is the root of all money? - Ayn Rand',
+    'So you think that money is the root of all evil. Have you ever asked what is the root of all money? \n\n- Ayn Rand',
 
-    'Do what you love and the money will follow. - Marsha Sinetar',
+    'Do what you love and the money will follow. \n\n- Marsha Sinetar',
 
-    'If I have enough money to eat I\'m good. Shia LaBeouf',
+    'If I have enough money to eat I\'m good. \n\n- Shia LaBeouf',
 
-    'Friendship is like money, easier made than kept. - Samuel Butler',
+    'Friendship is like money, easier made than kept. \n\n- Samuel Butler',
   ];
   const item = quotesArray[Math.floor(Math.random() * quotesArray.length)];
   return item;

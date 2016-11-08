360c096cd676d11b6bec94c35fa9d4c6701d8440
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-07 19:20:04 -0700

    Transition to different progress bar

--- app/components/summary/ProgressBar.js
+++ app/components/summary/ProgressBar.js
@@ -2,29 +2,38 @@ import React, { Component, PropTypes } from 'react';
 import {
   StyleSheet,
 } from 'react-native';
-import Svg, {
-    Rect,
-    Circle,
-    Path,
-} from 'react-native-svg';
+
+const Bar = require('react-native-progress-bar');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 class ProgressBar extends Component {
   render() {
-    const { income, recurring, expenses, savings, fundsAvailable } = this.props;
-    let base = income;
     return (
-      <Svg
-        height="100"
-        width="100"
-      >
-        <Rect x="0" y="0" width="50" height="25" fill="black" />
-        <Rect x="25" y="15" width="50" height="25" fill="red" />
-        <Circle cx="50" cy="50" r="30" fill="yellow" />
-        <Circle cx="40" cy="40" r="4" fill="black" />
-        <Circle cx="60" cy="40" r="4" fill="black" />
-        <Path d="M 40 60 A 10 10 0 0 0 60 60" stroke="black" />
-      </Svg>
-        );
+      <Bar
+        fillStyle={{}}
+        backgroundStyle={{backgroundColor: '#cccccc', borderRadius: 2}}
+        style={{marginTop: 10, width: 300}}
+        progress={this.state.progress}
+      />
+    )
   }
 }
 
@@ -36,4 +45,36 @@ ProgressBar.propTypes = {
   fundsAvailable: PropTypes.number.isRequired,
 };
 
+
 export default ProgressBar;
+
+// import Svg, {
+//     Rect,
+//     Circle,
+//     Path,
+// } from 'react-native-svg';
+//
+// class ProgressBar extends Component {
+//   render() {
+//     const { income, recurring, expenses, savings, fundsAvailable } = this.props;
+//     let base = income;
+//     return (
+//       <Svg
+//         height="100"
+//         width="100"
+//       >
+//         <Rect x="0" y="0" width={100 / 10} height="25" fill="black" />
+//         <Rect x="25" y="0" width="50" height="25" fill="red" />
+//         <Rect x="50" y="0" width="900" height="25" fill="purple" />
+//       </Svg>
+//         );
+//   }
+// }
+//
+// ProgressBar.propTypes = {
+//   income: PropTypes.number.isRequired,
+//   recurring: PropTypes.number,
+//   expenses: PropTypes.number,
+//   savings: PropTypes.number,
+//   fundsAvailable: PropTypes.number.isRequired,
+// };

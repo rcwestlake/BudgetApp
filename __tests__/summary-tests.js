68655ebaa3b76b812401136fd57fbf6c62ae040c
Gabi Procell <gprocell927@gmail.com>
2016-11-07 21:26:01 -0700

    Removes application tests, adds unit tests for Summary

---
+++ __tests__/summary-tests.js
@@ -0,0 +1,40 @@
+import { View,
+         Text,
+         ScrollView,
+         Image,
+          } from 'react-native';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect, assert } from 'chai';
+import Summary from '../app/components/summary/Summary';
+
+
+describe('Summary Component', () => {
+  beforeEach(() => {
+    wrapper = shallow(<Summary />);
+  });
+
+  it('renders within a ScrollView', () => {
+    expect(wrapper.type()).to.equal(ScrollView);
+  });
+
+  it('renders Text components', () => {
+    expect(wrapper.find(Text)).to.have.lengthOf(9);
+  });
+
+  it('renders buttons', () => {
+    expect(wrapper.find(TouchableHighlight)).to.have.length(3);
+  });
+  });
+
+  describe('Summary States', () => {
+    beforeEach(() => {
+      wrapper = shallow(<Summary />);
+    });
+    it('should set fundsAvailable to null by default', () => {
+      expect(wrapper.state().fundsAvailable).to.equal(null);
+    });
+    it('should set data to an empty string by default', () => {
+      expect(wrapper.state().data).to.equal('');
+    });
+  });
